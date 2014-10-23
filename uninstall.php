932042460547d1fb899bbc112bf5d922cdadcd64
mailmunch <mailmunch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-23 22:26:10 +0000

    fixed uninstall.php by adding php tags
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-forms-by-mailmunch/trunk@1013114 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,3 +1,4 @@
+<?php
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
     exit();
 
@@ -5,3 +6,4 @@ delete_option("mc_mm_data");
 delete_option("mc_mm_user_email");
 delete_option("mc_mm_user_password");
 delete_option("mc_mm_guest_user");
+?>
