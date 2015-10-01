cab5ab19898e254aba9c79242d393079be5c835c
mailmunch <mailmunch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-01 01:40:19 +0000

    Fixed text domain for language packs
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-forms-by-mailmunch/tags/2.1.2@1257069 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mailchimp-mailmunch.php
+++ mailchimp-mailmunch.php
@@ -21,7 +21,7 @@
  * Author URI:        http://www.mailmunch.co
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       mailchimp-mailmunch
+ * Text Domain:       mailchimp-forms-by-mailmunch
  * Domain Path:       /languages
  */
 
